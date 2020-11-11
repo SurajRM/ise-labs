a9272812134dcefae866092f2bb7f778072102bf
Uday <iamuday1618@gmail.com>
2020-11-11 16:21:30 +0530

    Refactor multilevel scheduling

--- OS/cpu_scheduling_multilevel.c
+++ OS/cpu_scheduling_multilevel.c
@@ -1,48 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define sf scanf
+#define pf printf
+#define max 10
+
+int p;
+void fcfs(int q[], int initialwaittime, int np)
+{
+    int turn[np], wait[np], turnsum = initialwaittime + q[0], waitsum = initialwaittime, i;
+    turn[0] = q[0] + initialwaittime;
+    wait[0] = initialwaittime;
+    for (i = 1; i < np; i++)
+    {
+        wait[i] = wait[i - 1] + q[i - 1];
+        turn[i] = wait[i] + q[i];
+        turnsum += turn[i];
+        waitsum += wait[i];
+    }
+    printf("\nProcess\t\tBurst time\t\tWaiting time\t\tTurnaround time\n");
+    for (int i = 0; i < np; i++)
+    {
+        printf("P%d\t\t%d\t\t\t%d\t\t\t%d\n", p++, q[i], wait[i], turn[i]);
+    }
+    printf("\nAverage Waiting time: %.2f\nAverage Turn-around time: %.2f\n\n\n", (float)waitsum / np, (float)turnsum / np);
+}
 
 int main()
 {
-    int Q1_count, Q2_count, Q1_priority, Q2_priority, time = 0, wait_time = 0, turnaround_time = 0;
-    printf("Enter the number of process in Q1: ");
-    scanf("%d", &Q1_count);
-    printf("Enter the number of process in Q2: ");
-    scanf("%d", &Q2_count);
-    int bt[Q1_count + Q2_count];
-    printf("Enter the priority for Q1 and Q2 respectively: ");
-    scanf("%d %d", &Q1_priority, &Q2_priority);
-    int queue_count = Q1_priority > Q2_priority ? Q1_count : Q2_count;
-    int queue = Q1_priority > Q2_priority ? 1 : 2;
-
-    for (int i = 0; i < queue_count; i++)
+    int p1, p2;
+    pf("Enter priority for Queue 1: ");
+    sf("%d", &p1);
+    pf("Enter priority for Queue 2: ");
+    sf("%d", &p2);
+    int np1, np2, i, w1 = 0, w2 = 0;
+    if (p1 == p2)
     {
-        printf("Enter the Burst Time for Q%d process %d: ", queue, i + 1);
-        scanf("%d", &bt[i]);
+        pf("Priority cannot be the same\n");
+        exit(0);
     }
-    for (int i = queue_count; i < Q1_count + Q2_count; i++)
+    pf("Enter the number of process of Queue 1: ");
+    sf("%d", &np1);
+    int q1[np1];
+    for (i = 0; i < np1; i++)
     {
-        printf("Enter the Burst Time for Q%d process %d: ", 3 - queue, i - queue_count + 1);
-        scanf("%d", &bt[i]);
+        pf("Enter the burst time for Process %d: ", i + 1);
+        sf("%d", &q1[i]);
+        w1 += q1[i];
     }
-    printf("\n\nProcess\t|Turnaround Time|Waiting Time\n");
-    for (int i = 0; i < Q1_count + Q2_count; i++)
+    pf("Enter the number of process of Queue 2: ");
+    sf("%d", &np2);
+    int q2[np2];
+    for (i = 0; i < np2; i++)
     {
-        time += bt[i];
-        wait_time += time - bt[i];
-        turnaround_time += time;
-        if (i < queue_count)
-            printf("P[%d]\t|\t%d\t|\t%d\n", i + 1, time, time - bt[i]);
-        else
-            printf("P[%d]\t|\t%d\t|\t%d\n", i - queue_count + 1, time, time - bt[i]);
-        if (i == queue_count - 1)
-        {
-            printf("\nAverage Waiting Time for Q%d = %0.2f\n", queue, (float)wait_time / queue_count);
-            printf("Average Turnaround Time for Q%d = %0.2f\n", queue, (float)turnaround_time / queue_count);
-            printf("\n\nProcess\t|Turnaround Time|Waiting Time\n");
-        }
-        else if (i == Q1_count + Q2_count - 1)
-        {
-            printf("\nAverage Waiting Time for Q%d = %0.2f\n", 3 - queue, (float)wait_time / (Q1_count + Q2_count - queue_count));
-            printf("Average Turnaround Time for Q%d = %0.2f\n", 3 - queue, (float)turnaround_time / (Q1_count + Q2_count - queue_count));
-        }
+        pf("Enter the burst time for Process %d: ", i + np1 + 1);
+        sf("%d", &q2[i]);
+        w2 += q2[i];
     }
+    if (p1 < p2)
+    {
+        pf("\nQUEUE 1:\n");
+        p = 1;
+        fcfs(q1, 0, np1);
+        pf("\nQUEUE 2:\n");
+        fcfs(q2, w1, np2);
+    }
+    else
+    {
+        pf("\nQUEUE 2:\n");
+        p = np1 + 1;
+        fcfs(q2, 0, np2);
+        pf("\nQUEUE 1:\n");
+        p = 1;
+        fcfs(q1, w2, np1);
+    }
+    return 0;
 }
