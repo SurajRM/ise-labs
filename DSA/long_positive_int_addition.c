613b23f9f11eaaec3aafd8be580caf0df70ad788
Uday <iamuday1618@gmail.com>
2020-11-24 22:57:27 +0530

    Use header node instead of tail pointer

--- DSA/long_positive_int_addition.c
+++ DSA/long_positive_int_addition.c
@@ -3,23 +3,28 @@
 #include <stdlib.h>
 
 typedef struct node {
-    int digit;
+    int data;
     struct node *next;
 } node;
 
 typedef struct CircularLL {
-    node *tail;
+    node *head;
 } CircularLL;
 
 void input_long_int(CircularLL *);
 node *create_node(int);
+void *create_circularll(CircularLL *);
 void insert_start(CircularLL *, int);
 void long_int_view(CircularLL *);
 void add_long_int(CircularLL *, CircularLL *, CircularLL *);
 void delete_circularll(CircularLL *);
 
 int main() {
-    CircularLL ans = {NULL}, L1 = {NULL}, L2 = {NULL};
+    CircularLL ans, L1, L2;
+    create_circularll(&L1);
+    create_circularll(&L2);
+    create_circularll(&ans);
+
     printf("            Addition of long positive integers using circular queue\n");
     input_long_int(&L1);
     input_long_int(&L2);
@@ -53,28 +58,37 @@ void input_long_int(CircularLL *LL) {
 
 node *create_node(int digit) {
     node *temp = (node *)malloc(sizeof(node));
-    temp->digit = digit;
+    temp->data = digit;
     temp->next = NULL;
     return temp;
 }
 
+void *create_circularll(CircularLL *LL) {
+    node *new_node = (node *)malloc(sizeof(node));
+    LL->head = new_node;
+    LL->head->data = 0;
+    LL->head->next = LL->head;
+}
+
 void insert_start(CircularLL *LL, int digit) {
     node *new_node = create_node(digit);
-    if (LL->tail == NULL) {
-        LL->tail = new_node;
-        new_node->next = new_node;
+    LL->head->data += 1;
+
+    if (LL->head->next == LL->head) {
+        LL->head->next = new_node;
+        new_node->next = LL->head;
         return;
     }
-    new_node->next = LL->tail->next;
-    LL->tail->next = new_node;
+    new_node->next = LL->head->next;
+    LL->head->next = new_node;
 }
 
 void long_int_view(CircularLL *LL) {
-    node *temp = LL->tail->next;
-    do {
-        printf("%d", temp->digit);
+    node *temp = LL->head->next;
+    while (temp != LL->head) {
+        printf("%d", temp->data);
         temp = temp->next;
-    } while (temp != LL->tail->next);
+    }
     printf("\n");
 }
 
@@ -82,35 +96,38 @@ void add_long_int(CircularLL *ans, CircularLL *L1, CircularLL *L2) {
     int carry = 0, sum, digit;
     node *left_over, *tail_node;
 
-    if (!L1->tail && !L2->tail)
+    if (L1->head->data == 0 && L2->head->data == 0)
         return;
-    if (!L1->tail)
+    if (L1->head->data == 0) {
         ans = L2;
-    if (!L2->tail)
+        return;
+    } else if (L2->head->data == 0) {
         ans = L1;
+        return;
+    }
 
-    node *p = L1->tail->next;
-    node *q = L2->tail->next;
-    do {
-        sum = p->digit + q->digit + carry;
+    node *p = L1->head->next;
+    node *q = L2->head->next;
+    while (p != L1->head && q != L2->head) {
+        sum = p->data + q->data + carry;
         digit = sum % 10;
         carry = sum / 10;
 
         insert_start(ans, digit);
         p = p->next;
         q = q->next;
-    } while (p != L1->tail->next && q != L2->tail->next);
-
-    if (p != L1->tail->next) {
-        left_over = p;
-        tail_node = L1->tail;
-    } else {
-        left_over = q;
-        tail_node = L2->tail;
     }
 
-    while (left_over != tail_node->next) {
-        sum = left_over->digit + carry;
+    if (p != L1->head) {
+        left_over = p;
+        tail_node = L1->head;
+    } else {
+        left_over = q;
+        tail_node = L2->head;
+    }
+
+    while (left_over != tail_node) {
+        sum = left_over->data + carry;
         digit = sum % 10;
         carry = sum / 10;
         insert_start(ans, digit);
@@ -122,11 +139,9 @@ void add_long_int(CircularLL *ans, CircularLL *L1, CircularLL *L2) {
 }
 
 void delete_circularll(CircularLL *LL) {
-    if (LL->tail == NULL)
-        return;
     node *next_node = NULL;
-    node *current_node = LL->tail->next;
-    while (current_node != LL->tail) {
+    node *current_node = LL->head->next;
+    while (current_node != LL->head) {
         next_node = current_node->next;
         free(current_node);
         current_node = next_node;
