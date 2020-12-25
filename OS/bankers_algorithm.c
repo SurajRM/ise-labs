bfc4b5877e812322c77cd211d1fbdfc8f719b17f
Uday <iamuday1618@gmail.com>
2020-12-25 21:23:33 +0530

    Implement is_safe function Small fixes

--- OS/bankers_algorithm.c
+++ OS/bankers_algorithm.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int **input_allocation_matrix(int, int);
 
@@ -7,7 +8,7 @@ int **input_max_matrix(int, int);
 
 int *input_available_vector(int);
 
-bool is_safe(int **, int **, int *, int *, int *);
+bool is_safe(int **, int **, int *, int, int);
 
 int main() {
     int process_count, resource_count;
@@ -20,7 +21,7 @@ int main() {
     scanf("%d", &resource_count);
     available = input_available_vector(resource_count);
     max = input_max_matrix(process_count, resource_count);
-    allocation = input_max_matrix(process_count, resource_count);
+    allocation = input_allocation_matrix(process_count, resource_count);
 
     return 0;
 }
@@ -61,4 +62,38 @@ int **input_max_matrix(int process_count, int resource_count) {
     }
 
     return max;
+}
+
+bool is_safe(int **allocation, int **max, int *available, int process_count, int resource_count) {
+    int i, j, k, finished = 0;
+    bool flag;
+
+    int finish[process_count], work[resource_count];
+
+    for (i = 0; i < process_count; i++)
+        finish[i] = false;
+    for (i = 0; i < resource_count; i++)
+        work[i] = available[i];
+
+    for (i = 0; i < process_count; i++) {
+        for (j = 0; j < process_count; j++) {
+            if (!finish[j]) {
+                flag = false;
+                for (k = 0; k < resource_count; k++) {
+                    if ((max[j][k] - allocation[j][k]) > work[k])
+                        flag = true;
+                }
+                if (flag) {
+                    for (k = 0; k < resource_count; k++)
+                        work[k] += allocation[j][k];
+                    finish[j] = true;
+                    finished++;
+                }
+            }
+
+        }
+        if (finished == process_count)
+            return true;
+    }
+    return false;
 }
