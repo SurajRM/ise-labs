b85fd951ce5a30bf7f535be24d6793684cacb0a5
Uday <iamuday1618@gmail.com>
2020-12-25 19:53:55 +0530

    Restructure the code

--- OS/bankers_algorithm.c
+++ OS/bankers_algorithm.c
@@ -1,56 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void input_allocation_vector(int *, const int *);
+int **input_allocation_matrix(int, int);
 
-void input_max_vector(int *, const int *);
+int **input_max_matrix(int, int);
 
-void input_available_vector(int *, const int *);
+int *input_available_vector(int);
 
-void input_data(int **, int **, int *, int *, int *);
+bool is_safe(int **, int **, int *, int *, int *);
 
 int main() {
     int process_count, resource_count;
-    int **allocation = NULL, **max = NULL, *available = NULL;
+    int **allocation, **max, *available;
+
     printf("\t\tDeadlock avoidance using Banker's Algorithm.\n");
-    input_data(allocation, max, available, &process_count, &resource_count);
+    printf("Enter the number of processes: ");
+    scanf("%d", &process_count);
+    printf("Enter the number of resource: ");
+    scanf("%d", &resource_count);
+    available = input_available_vector(resource_count);
+    max = input_max_matrix(process_count, resource_count);
+    allocation = input_max_matrix(process_count, resource_count);
+
     return 0;
 }
 
-void input_allocation_vector(int *allocation_vector, const int *resource_count) {
-    for (int i = 0; i < *resource_count; i++) {
-        printf("Allocation for resource: %d", i + 1);
-        scanf("%d", &allocation_vector[i]);
-    }
-}
-
-void input_max_vector(int *max_vector, const int *resource_count) {
-    for (int i = 0; i < *resource_count; i++) {
-        printf("Maximum demand for resource: %d", i + 1);
-        scanf("%d", &max_vector[i]);
-    }
-}
-
-void input_available_vector(int *available_vector, const int *resource_count) {
-    for (int i = 0; i < *resource_count; i++) {
+int *input_available_vector(int resource_count) {
+    int *available_vector = (int *) malloc(resource_count * sizeof(int));
+    for (int i = 0; i < resource_count; i++) {
         printf("Enter the number of available instances for resource %d", i + 1);
         scanf("%d", &available_vector[i]);
     }
+
+    return available_vector;
 }
 
-void input_data(int **allocation, int **max, int *available, int *process_count, int *resource_count) {
-    printf("Enter the number of processes: ");
-    scanf("%d", process_count);
-    printf("Enter the number of resource: ");
-    scanf("%d", resource_count);
-
-    allocation = (int **) malloc(*process_count * sizeof(int *));
-    max = (int **) malloc(*process_count * sizeof(int *));
-    for (int i = 0; i < *process_count; i++) {
-        allocation[i] = (int *) malloc(*resource_count * sizeof(int));
+int **input_allocation_matrix(int process_count, int resource_count) {
+    int **allocation = (int **) malloc(process_count * sizeof(int *));
+    for (int i = 0; i < process_count; i++) {
         printf("\nProcess %d:\n", i + 1);
-        input_allocation_vector(allocation[i], resource_count);
-        input_max_vector(max[i], resource_count);
+        allocation[i] = (int *) malloc(resource_count * sizeof(int));
+        for (int j = 0; j < resource_count; j++) {
+            printf("Allocation for resource: %d", j + 1);
+            scanf("%d", &allocation[i][j]);
+        }
     }
-    input_available_vector(available, resource_count);
+
+    return allocation;
+}
+
+int **input_max_matrix(int process_count, int resource_count) {
+    int **max = (int **) malloc(process_count * sizeof(int *));
+    for (int i = 0; i < process_count; i++) {
+        printf("\nProcess %d:\n", i + 1);
+        max[i] = (int *) malloc(resource_count * sizeof(int *));
+        for (int j = 0; j < resource_count; j++) {
+            printf("Maximum demand for resource: %d", j + 1);
+            scanf("%d", &max[i][j]);
+        }
+    }
+
+    return max;
 }
