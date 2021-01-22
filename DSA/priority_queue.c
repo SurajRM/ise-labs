d6487ec713e7aecbcc82892eae1e4dccdeab2b3f
Uday <iamuday1618@gmail.com>
2021-01-22 14:57:02 +0530

    Add more priority queue functionalities

--- DSA/priority_queue.c
+++ DSA/priority_queue.c
@@ -10,12 +10,18 @@ typedef struct heap {
 
 Heap *create_heap(int);
 bool insert(Heap *, int);
+int parent(int);
+int left_child(int);
+int right_child(int);
+void shift_up(Heap *, int);
+void shift_down(Heap *, int);
 bool delete(Heap *, int);
 int get_max(Heap *);
 int pop_max(Heap *);
 void print_queue(Heap *);
 bool is_empty(Heap *);
 bool is_full(Heap *);
+void swap(int *, int *);
 
 int main() {
     return 0;
@@ -49,4 +55,74 @@ Heap *create_heap(int capacity) {
     hp->capacity = capacity;
     hp->size = -1;
     return hp;
+}
+
+bool insert(Heap *hp, int value) {
+    if (is_full(hp)) {
+        printf("error: heap full\n");
+        return false;
+    }
+    hp->arr[++(hp->size)] = value;
+    shift_up(hp, hp->size);
+    return true;
+}
+
+int pop_max(Heap *hp) {
+    int max = hp->arr[0];
+    hp->arr[0] = hp->arr[hp->size--];
+    shift_down(hp, 0);
+    return max;
+}
+
+bool delete(Heap *hp, int position) {
+    if (is_empty(hp)) {
+        printf("error: heap empty\n");
+        return false;
+    }
+    hp->arr[position] = get_max(hp) + 1;
+    shift_up(hp, position);
+    pop_max(hp);
+    return true;
+}
+
+void shift_up(Heap *hp, int pos) {
+    while (pos > 0 && hp->arr[parent(pos)] < hp->arr[pos]) {
+        swap(&hp->arr[parent(pos)], &hp->arr[pos]);
+        pos = parent(pos);
+    }
+}
+
+void shift_down(Heap *hp, int pos) {
+    int max_index = pos;
+
+    int left_index = left_child(pos);
+    if (left_index <= hp->size && hp->arr[left_index] > hp->arr[max_index])
+        max_index = left_index;
+
+    int right_index = right_child(pos);
+    if (right_index <= hp->size && hp->arr[right_index] > hp->arr[max_index])
+        max_index = right_index;
+
+    if (pos != max_index) {
+        swap(&hp->arr[pos], &hp->arr[max_index]);
+        shift_down(hp, max_index);
+    }
+}
+
+void swap(int *a, int *b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+
+int parent(int i) {
+    return (i - 1) / 2;
+}
+
+int left_child(int i) {
+    return 2 * i + 1;
+}
+
+int right_child(int i) {
+    return 2 * i + 2;
 }
