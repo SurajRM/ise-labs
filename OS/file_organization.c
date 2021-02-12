1091de2a7f380e30cb718cdab1314928daf1e26a
Uday <iamuday1618@gmail.com>
2021-02-12 19:29:14 +0530

    Restructure code

--- OS/file_organization.c
+++ OS/file_organization.c
@@ -3,23 +3,27 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#define STRING_SIZE 512
-#define MAX_FILE_COUNT 512
+#define STRING_SIZE 255
+#define MAX_FILE_COUNT 255
+#define MAX_DIR_COUNT 255
 
 typedef struct File {
     char file_name[STRING_SIZE];
 } File;
 
-typedef struct SingleLevelTable {
+typedef struct directory {
+    char dir_name[STRING_SIZE];
     File *files[MAX_FILE_COUNT];
+    struct Directory *directories[MAX_DIR_COUNT];
     int file_count;
-} SingleLevelTable;
+    int dir_count;
+} Directory;
 
 File *new_file(char *);
-bool search_file(SingleLevelTable *, char *file);
-void insert_file(SingleLevelTable *, char *file);
-void display_files(SingleLevelTable *);
-SingleLevelTable *create_slt();
+Directory *new_directory(char *);
+bool search_file(Directory *, char *file);
+void insert_file(Directory *, char *file);
+void display_files(Directory *);
 void single_level();
 
 int main() {
@@ -36,49 +40,42 @@ File *new_file(char *file_name) {
     return new_file;
 }
 
-bool search_file(SingleLevelTable *slt, char *file) {
-    for (int i = 0; i < slt->file_count; i++) {
-        if (slt->files[i] != NULL && strcmp(slt->files[i]->file_name, file) == 0)
+bool search_file(Directory *dir, char *file) {
+    for (int i = 0; i < dir->file_count; i++) {
+        if (dir->files[i] != NULL && strcmp(dir->files[i]->file_name, file) == 0)
             return true;
     }
     return false;
 }
 
-void insert_file(SingleLevelTable *slt, char *file) {
-    if (search_file(slt, file) == true) {
-        printf("File %s already exists\n", file);
+void insert_file(Directory *dir, char *file) {
+    if (search_file(dir, file) == true) {
+        printf("File \"%s\" already exists\n", file);
         return;
     }
-    slt->files[slt->file_count] = new_file(file);
-    slt->file_count++;
+    dir->files[dir->file_count] = new_file(file);
+    dir->file_count++;
     printf("File created\n");
 }
 
-void display_files(SingleLevelTable *slt) {
+void display_files(Directory *dir) {
     printf("Contents of root:\n");
-    if (slt->file_count == 0) {
+    if (dir->file_count == 0) {
         printf("Empty!\n");
         return;
     }
-    for (int i = 0; i < slt->file_count; i++) {
-        if (slt->files[i] != NULL)
-            printf("%s\t", slt->files[i]->file_name);
+    for (int i = 0; i < dir->file_count; i++) {
+        if (dir->files[i] != NULL)
+            printf("%s\t", dir->files[i]->file_name);
     }
     printf("\n");
-}
-
-SingleLevelTable *create_slt() {
-    SingleLevelTable *slt = (SingleLevelTable *) malloc(sizeof(SingleLevelTable));
-    slt->file_count = 0;
-    for (int i = 0; i < MAX_FILE_COUNT; i++)
-        slt->files[i] = NULL;
 }
 
 void single_level() {
     printf("Single-Level directory\n");
     int choice;
     char file[STRING_SIZE];
-    SingleLevelTable *slt = create_slt();
+    Directory *root = new_directory("root");
 
     while (true) {
         printf("1: Create new file\n");
@@ -89,13 +86,26 @@ void single_level() {
         if (choice == 1) {
             printf("Enter file name: ");
             scanf("%s", file);
-            insert_file(slt, file);
-            display_files(slt);
+            insert_file(root, file);
+            display_files(root);
         } else if (choice == 2)
-            display_files(slt);
+            display_files(root);
         else
             break;
         printf("\n");
     }
+}
+
+Directory *new_directory(char *dir_name) {
+    Directory *new_dir = (Directory *) malloc(sizeof(Directory));
+    strcpy(new_dir->dir_name, dir_name);
+    new_dir->dir_count = 0;
+    new_dir->file_count = 0;
+    for (int i = 0; i < MAX_FILE_COUNT; i++)
+        new_dir->files[i] = NULL;
+    for (int i = 0; i < MAX_DIR_COUNT; i++)
+        new_dir->directories[i] = NULL;
+
+    return new_dir;
 }
 
