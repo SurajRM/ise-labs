99c94cb6b6ab560269a22ce1c030d20919e334e4
Uday <iamuday1618@gmail.com>
2021-02-14 14:20:10 +0530

    Add more functionalities
    - Add option to get top third element
    - Add option to get bottom third element
    - Add option to pop top n elements

--- DSA/stack.c
+++ DSA/stack.c
@@ -15,6 +15,9 @@ int peek(Stack *);
 void display_items(Stack *);
 void push(Stack *, int);
 int pop(Stack *);
+int get_top_third_ele(Stack *);
+int get_bottom_third_ele(Stack *st);
+void pop_top_n_ele(Stack *, int);
 
 int main() {
     int capacity, choice, temp, item;
@@ -35,6 +38,9 @@ int main() {
         printf("4: Check if stack is full\n");
         printf("5: Check if stack is empty\n");
         printf("6: Display items in stack\n");
+        printf("7: Get the 3rd element from top of the stack\n");
+        printf("8: Pop out top n elements from the stack\n");
+        printf("9: Get 3rd element from bottom of the stack\n");
         printf("-1: Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
@@ -73,6 +79,29 @@ int main() {
                 break;
             case 6:
                 display_items(st);
+                break;
+            case 7:
+                temp = get_top_third_ele(st);
+                if (temp == -2)
+                    printf("The top third element does not exist\n");
+                else
+                    printf("The third element from top of the stack is: %d\n", temp);
+                break;
+            case 8:
+                printf("Enter number of elements to be popped from the stack: ");
+                scanf("%d", &temp);
+                while (temp > st->top + 1) {
+                    printf("The number of elements in the stack is %d, try again: ", st->top + 1);
+                    scanf("%d", &temp);
+                }
+                pop_top_n_ele(st, temp);
+                break;
+            case 9:
+                temp = get_bottom_third_ele(st);
+                if (temp == -2)
+                    printf("The bottom third element does not exist\n");
+                else
+                    printf("The third element from bottom of the stack is: %d\n", temp);
                 break;
             case -1:
                 exit(0);
@@ -134,4 +163,48 @@ int pop(Stack *s) {
     if (!is_empty(s))
         return s->items[--s->top + 1];
     return -2;
+}
+
+int get_top_third_ele(Stack *st) {
+    Stack *temp = create_stack(3);
+    int top_3rd_ele;
+
+    if (st->top >= 2) {
+        for (int i = 0; i < 3; i++)
+            push(temp, pop(st));
+        top_3rd_ele = peek(temp);
+
+        for (int i = 0; i < 3; i++)
+            push(st, pop(temp));
+
+        return top_3rd_ele;
+    }
+    free(temp);
+    return -2;
+}
+
+int get_bottom_third_ele(Stack *st) {
+    Stack *temp = create_stack(st->top - 1);
+    int bottom_3rd_ele;
+
+    if (st->top >= 2) {
+        for (int i = st->top; i >= 2; i--)
+            push(temp, pop(st));
+        bottom_3rd_ele = peek(temp);
+
+        for (int i = temp->top; i >= 0; i--)
+            push(st, pop(temp));
+
+        return bottom_3rd_ele;
+    }
+    free(temp);
+    return -2;
+}
+
+void pop_top_n_ele(Stack *st, int n) {
+    for (int i = 0; i < n; i++)
+        pop(st);
+
+    printf("After popping %d elements\n", n);
+    display_items(st);
 }
