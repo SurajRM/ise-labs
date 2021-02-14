97cfcb0fbf3b1967da66a5904a824b2bc104ed00
Uday <iamuday1618@gmail.com>
2021-02-14 18:24:08 +0530

    Bug Fixes and Improvements

--- DSA/infix_to_postfix.c
+++ DSA/infix_to_postfix.c
@@ -4,62 +4,61 @@
 #include <string.h>
 #include <stdbool.h>
 
-char infix_string[200], postfix_string[200];
-int top;
-int stack[200];
+#define MAX_LEN 512
 
-int pop();
+int top;
+char infix_string[MAX_LEN], postfix_string[MAX_LEN], stack[MAX_LEN];
+
+char pop();
 int precedence(char);
-int is_empty();
+bool is_empty();
 void infix_to_postfix();
-int check_space(char);
-void push(long int);
+void push(char);
 bool validity(int, int);
 bool is_operator(char);
 
 int main() {
     top = -1;
-    printf("INPUT THE INFIX EXPRESSION : ");
+    printf("Input the infix expression: ");
     scanf("%s", infix_string);
-    if (!validity(0, strlen(infix_string) - 1)) {
-        printf("Invalid input\n");
+
+    if (!validity(0, (int) strlen(infix_string) - 1)) {
+        printf("Invalid infix expression\n");
         exit(0);
     }
+
     infix_to_postfix();
-    printf("\nEQUIVALENT POSTFIX EXPRESSION : %s\n", postfix_string);
+    printf("Equivalent postfix expression: %s\n", postfix_string);
     return 0;
 }
 
 void infix_to_postfix() {
-    unsigned int count, temp = 0;
+    int count, temp = 0;
     char next;
     char symbol;
     for (count = 0; count < strlen(infix_string); count++) {
         symbol = infix_string[count];
-        if (!check_space(symbol)) {
-            switch (symbol) {
-                case '(':
-                    push(symbol);
-                    break;
-                case ')':
-                    while ((next = pop()) != '(') {
-                        postfix_string[temp++] = next;
-                    }
-                    break;
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                case '%':
-                case '^':
-                    while (!is_empty() &&
-                           precedence(stack[top]) >= precedence(symbol)) // Check precedence and push the higher one
-                        postfix_string[temp++] = pop();
-                    push(symbol);
-                    break;
-                default:
-                    postfix_string[temp++] = symbol;
-            }
+        switch (symbol) {
+            case '(':
+                push(symbol);
+                break;
+            case ')':
+                while ((next = pop()) != '(') {
+                    postfix_string[temp++] = next;
+                }
+                break;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '%':
+            case '^':
+                while (!is_empty() && precedence(stack[top]) >= precedence(symbol))
+                    postfix_string[temp++] = pop();
+                push(symbol);
+                break;
+            default:
+                postfix_string[temp++] = symbol;
         }
     }
     while (!is_empty()) {
@@ -86,50 +85,39 @@ int precedence(char symbol) {
     }
 }
 
-int check_space(char symbol) {
-    if (symbol == '\t' || symbol == ' ') {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-void push(long int symbol) {
-    if (top > 20) {
-        printf("Stack Overflow\n");
+void push(char symbol) {
+    if (top >= MAX_LEN) {
+        printf("error: stack is full\n");
         exit(1);
     }
-    top = top + 1;
-    stack[top] = symbol;
+    stack[++top] = symbol;
 }
 
-int is_empty() {
-    if (top == -1) {
-        return 1;
-    } else {
-        return 0;
-    }
+bool is_empty() {
+    if (top == -1)
+        return true;
+    return false;
 }
 
-int pop() {
+char pop() {
     if (is_empty()) {
-        printf("Stack is Empty\n");
+        printf("error: stack is empty\n");
         exit(1);
     }
     return (stack[top--]);
 }
 
 bool validity(int start, int end) {
-    int len = end - start + 1;
-    bool v1, v2;
+    int len = end - start + 1, paren;
 
     if (len == 0 || infix_string[start] == ')')
         return false;
     if (len == 1 && isalpha(infix_string[start]))
         return true;
+
     if (infix_string[start] == '(') {
-        int paren = 1;
-        for (int i = start + 1; i < end + 1; i++) {
+        paren = 1;
+        for (int i = start + 1; i <= end; i++) {
             if (infix_string[i] == '(')
                 paren++;
             else if (infix_string[i] == ')')
@@ -139,27 +127,20 @@ bool validity(int start, int end) {
                     return validity(start + 1, end - 1);
                 else if (!is_operator(infix_string[i + 1]))
                     return false;
-                else {
-                    v1 = validity(start + 1, i - 1);
-                    v2 = validity(i + 2, end);
-                    break;
-                }
+                else
+                    return validity(start + 1, i - 1) && validity(i + 2, end);
             }
         }
-        if (paren != 0)
-            return false;
+        return false;
     } else {
         for (int i = start; i <= end; i++) {
             if (infix_string[i] == '(' || infix_string[i] == ')')
                 return false;
-            if (is_operator(infix_string[i])) {
-                v1 = validity(start, i - 1);
-                v2 = validity(i + 1, end);
-                break;
-            }
+            if (is_operator(infix_string[i]))
+                return validity(start, i - 1) && validity(i + 1, end);
         }
     }
-    return v1 == true && v2 == true;
+    return false;
 }
 
 bool is_operator(char symbol) {
